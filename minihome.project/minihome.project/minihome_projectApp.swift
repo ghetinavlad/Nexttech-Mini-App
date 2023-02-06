3668e07d476946a97fefd5afa1172d9e31b10784
ghetinavlad <73642253+ghetinavlad@users.noreply.github.com>
2023-02-06 19:52:25 +0200

    Setup project

---
+++ minihome.project/minihome.project/minihome_projectApp.swift
@@ -0,0 +1,17 @@
+//
+//  minihome_projectApp.swift
+//  minihome.project
+//
+//  Created by vladut on 2/6/23.
+//
+
+import SwiftUI
+
+@main
+struct minihome_projectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
