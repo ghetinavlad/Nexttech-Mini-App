44d0e8b2b6e31033fdbea23d023bfdaf42b95d86
ghetinavlad <73642253+ghetinavlad@users.noreply.github.com>
2023-02-10 12:45:37 +0200

    Rename APIClient to DataSource and refactor the file

--- minihome.project/minihome.project/DailyGratitude/DailyGratitudeList/DailyGratitudeListRepository.swift
+++ minihome.project/minihome.project/DailyGratitude/DailyGratitudeList/DailyGratitudeListRepository.swift
@@ -8,7 +8,7 @@
 import Foundation
 import Combine
 
-protocol DailyGratitudeListRepository: APIClient {
+protocol DailyGratitudeListRepository: DataSource {
     func getDailyGratitudeList() -> AnyPublisher<[JournalTypeEntry], Error>
 }
 
